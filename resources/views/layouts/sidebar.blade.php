61f858f1cce21b2b4f58ebf1550d758a2a2aaeb1
Mushfequr Rahman <me.vgithub@gmail.com>
2025-08-09 11:07:27 +0600

    delete logout li from sidebar.blade.php

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -67,12 +67,7 @@
                 {{-- Dynamic Modules --}}
                 {!! renderSidebarMenu($modules, $permissions) !!}
 
-                <li class="nav-item">
-                    <a href="{{ route('logout') }}" class="nav-link">
-                        <i class="nav-icon fas fa-sign-out-alt"></i>
-                        <p>Logout</p>
-                    </a>
-                </li>
+                
             </ul>
         </nav>
         <!-- /.sidebar-menu -->
